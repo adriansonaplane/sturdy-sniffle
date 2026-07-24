40f15c60fd7b07fcb37b858c7594142499f2fc1a
adriansonaplane <109830657+adriansonaplane@users.noreply.github.com>
2026-07-23 19:47:41 -0700

    Implement Asset Forge governance registry contracts

--- src/workbench/asset-forge/index.ts
+++ src/workbench/asset-forge/index.ts
@@ -1,1 +1,2 @@
 export * from './core/index.js';export * from './contracts/index.js';export * from './registries/index.js';export * from './integration/dungeonRegistryBoundary.js';
+export * from './lifecycle/acceptanceStateMachine.js';export * from './lifecycle/transitionPolicy.js';export * from './lifecycle/supersession.js';export * from './integration/assetForgeCapabilities.js';
--- src/workbench/asset-forge/registries/index.ts
+++ src/workbench/asset-forge/registries/index.ts
@@ -1,1 +1,2 @@
 export * from './environmentPackRegistry.js';export * from './generatorRegistry.js';export * from './materialProfileRegistry.js';export * from './requirementRegistry.js';export * from './styleProfileRegistry.js';
+export * from './validationRuleRegistry.js';export * from './runtimeAssetRegistry.js';
